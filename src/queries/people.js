32f9b57e25f076827e9491aff080df548b11f22e
Byron Lopez <merlin2010@gmail.com>
2021-02-06 22:47:13 -0600

    Create a schema to get the user bio information data by username Fixes #6
    - Added getBiosByUsername method.
    - Added Bios type schema.

--- src/queries/people.js
+++ src/queries/people.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server');
-const { postData } = require('../axios');
+const { postData, getData } = require('../axios');
 
 module.exports = {
   getPeople: async (_, { offset, size }, { isAuthenticated }) => {
@@ -10,6 +10,15 @@ module.exports = {
     const { data } = await postData(
       `${process.env.JBM_TORRE_API_URL}/people/_search/?offset=${offset}&size=${size}`
     );
+
+    return data;
+  },
+  getBioByUsername: async (_, { username }, { isAuthenticated }) => {
+    if (!isAuthenticated) {
+      throw new AuthenticationError('must authenticate');
+    }
+
+    const { data } = await getData(`${process.env.JBM_TORRE_API_URL_BIOS}/${username}`);
 
     return data;
   },
